ea6a82c16a6303dda1584673a611b8827d46066c
Weston Ellis <ellisweston112@gmail.com>
2021-10-23 14:47:33 -0600

    Add relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
-RSpec.describe Item do
+RSpec.describe Invoice do
   describe 'relationships' do
+    it { should belong_to(:customer) }
     it { should belong_to(:merchant) }
     it { should have_many(:invoice_items) }
-    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:items).through(:invoice_items) }
   end
 end
