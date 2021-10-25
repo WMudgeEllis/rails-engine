de5ebd25a4cfe8c1f150962569575e08f64f6cd9
Weston Ellis <ellisweston112@gmail.com>
2021-10-25 13:37:27 -0600

    Write test for item by name

--- spec/api/v1/item_service_spec.rb
+++ spec/api/v1/item_service_spec.rb
@@ -182,4 +182,27 @@ RSpec.describe 'item api endpoints' do
     expect(Item.all).to eq([item2])
     expect(Invoice.all).to eq([invoice2])
   end
+
+  it 'can search for an item by name' do
+    merchant = create(:merchant)
+    create_list(:item, 10, merchant_id: merchant.id)
+    item = create(:item, name: 'Westons awesome item')
+
+    get '/api/vi/items/find?name=West'
+
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body).to be_a(Hash)
+    expect(body).to have_key(:data)
+    expect(body[:data]).to be_a(Hash)
+    expect(body[:data]).to have_key(:id)
+    expect(body[:data][:id]).to eq(item.id.to_s)
+    expect(body[:data]).to have_key(:type)
+    expect(body[:data][:type]).to be_a(String)
+    expect(body[:data]).to have_key(:attributes)
+    expect(body[:data][:attributes]).to be_a(Hash)
+    expect(body[:data][:name]).to eq(item.name)
+  end
 end
