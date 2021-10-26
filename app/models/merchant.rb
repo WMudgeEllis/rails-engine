dfd4d3064325503cde6942ad43c4f568a97b91c3
Weston Ellis <ellisweston112@gmail.com>
2021-10-26 12:10:24 -0600

    refactor

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,13 +8,10 @@ class Merchant < ApplicationRecord
   end
 
   def total_revenue
-    invoice_items = invoices.where(status: 'shipped')
-                            .joins(:transactions)
-                            .where(transactions: {result: 'success'})
-                            .joins(:invoice_items)
-                            .select('invoice_items.*')
-    invoice_items.sum do |invoice_item|
-       invoice_item.quantity * invoice_item.unit_price
-     end
+    invoices.joins(:transactions, :invoice_items)
+            .where(transactions: {result: 'success'})
+            .where(invoices: {status: 'shipped'})
+            .select('invoice_items.*')
+            .sum('unit_price * quantity')
   end
 end
